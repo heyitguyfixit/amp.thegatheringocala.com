97aacc02bfd508e5809c6ed3ae18236a2eb9e5d6
heyitguyfixit <34974905+heyitguyfixit@users.noreply.github.com>
2018-08-17 22:58:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ for (i = 0; i < drop.length; i++) {
     drop[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var sub = this.nextElementSibling;
-        if (sub.style.maxHeight === "1000px") {
+        if (sub.style.maxHeight === "200px") {
             sub.style.maxHeight = "0px";
         } else {
-            sub.style.maxHeight = "1000px";
+            sub.style.maxHeight = "200px";
         }
     });
 }
